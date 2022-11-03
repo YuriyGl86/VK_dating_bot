e293621a2931e56213cd7c910910992e5b83f421
Mark Izotov <iztvmrk@mail.ru>
2022-11-03 23:14:50 +0300

    interaction

--- modules.py
+++ modules.py
@@ -1,33 +1,46 @@
 import configparser
 import requests
+from bot.bot import Bot
+from bot.bot_token import key
+import datetime
+from DB.db import rec_favorites, rec_blocked
 
 config = configparser.ConfigParser()
 config.read('token.ini')
 TOKEN = config['VK_API']['access_token']
 
-class Candidate_Selection():
+class Candidate_selection():
     
     def __init__(
         self,
-        user_age,
-        user_sex,
-        user_city,
         user_access_token = TOKEN,
         version = '5.131',
         album = 'profile',
         extended = int(True),
         rev = int(False)
         ):
-        self.UA = user_age
-        self.US = user_sex
-        self.UC = user_city
         self.UAT = user_access_token
         self.V = version
         self.A = album
         self.EX = extended
         self.R = rev
         
-    def candidate_parametrs(self):
+    def take_user_all_info(self) -> int:
+        user_info = Bot(key).get_user_info(501244677)
+        return user_info        
+        
+    def candidate_parametrs(self) -> dict:
+        try:
+            if self.take_user_all_info()['sex'] == 1:
+                natural_sex = 2
+            elif self.take_user_all_info()['sex'] == 2:
+                natural_sex = 1
+            else:
+                KeyError
+        except KeyError:
+            return 'Не определён пол'
+        candidate_city = self.take_user_all_info()['city']['title']
+        user_age = datetime.datetime.now().year - int(self.take_user_all_info()['bdate'][4:])
         url = 'https://api.vk.com/method/users.search'
         response = requests.get(
             url,
@@ -35,19 +48,18 @@ class Candidate_Selection():
                 'is_closed': False,
                 'can_access_closed': False,
                 'access_token': self.UAT,
-                'fields': 'city, bdate',
-                'age_from': self.UA - 3,
-                'age_to': self.UA + 3,
-                'hometown': self.UC,
-                'sex': int(not(self.US)),
-                'v': '5.131',
+                'fields': 'city, bdate, sex',
+                'age_from': user_age - 3,
+                'age_to': user_age + 3,
+                'hometown': candidate_city,
+                'sex': natural_sex,
+                'v': self.V,
                 'count': 1
             }
         )
         return response.json()
-
-        
-    def get_parametrs_to_search(self):
+     
+    def get_parametrs_to_search(self) -> dict:
         for own_id in self.candidate_parametrs().values():
             candidat_id = own_id['items'][0]['id']
         url = 'https://api.vk.com/method/photos.get'
@@ -65,7 +77,7 @@ class Candidate_Selection():
         )
         return response.json()
 
-    def candidate_photo(self):
+    def candidate_photo(self) -> dict:
         # метод выборки фотографий с наибольшей популярностью
         list_likes = []
         list_info = []
@@ -86,9 +98,26 @@ class Candidate_Selection():
                 url_list.append(url)
         return {'photo': url_list}
 
-    def unification_info(self):
+    def unification_info(self) -> dict:
         get_params_info = self.candidate_parametrs().get('response')['items'][0]
         get_params_info.update(self.candidate_photo())
         return get_params_info
 
-print(Candidate_Selection(18, bool(0), 'Севастополь').unification_info())
+# print(Candidate_selection().unification_info())
+
+class Checking_for_id():
+    def __init__(
+        self,
+        ignore_list = rec_blocked(),
+        favorite_list = rec_favorites(),
+        candidate_id = Candidate_selection.unification_info()['id']
+        ):
+        self.IL = ignore_list
+        self.FL = favorite_list
+        self.CID = candidate_id
+    
+    def checking_lists(self):
+        if self.CID not in self.IL and self.FL:
+            return Candidate_selection().unification_info()
+        
+print(Checking_for_id().checking_lists())
