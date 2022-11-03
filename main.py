9b724d750d4f17eecbd392e4d4c1012087d48089
Mark Izotov <iztvmrk@mail.ru>
2022-11-04 00:06:07 +0300

    bot completed v1.1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import configparser
 # from DB.db import rec_favorites, rec_blocked, rec_vk_user
 
 config = configparser.ConfigParser()
-config.read('token.ini')
+config.read('token_new.ini')
 KEY = config['VK_API']['key_oauth']
 
 def main():
