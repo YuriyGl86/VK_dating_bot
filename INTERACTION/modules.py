e04ecee56999c544fa7679904a191d33c3392b5c
Mark Izotov <iztvmrk@mail.ru>
2022-11-03 04:21:12 +0300

    main

--- INTERACTION/modules.py
+++ INTERACTION/modules.py
@@ -1,66 +1,82 @@
-import json
+import configparser
 import requests
-class Interaction():
+
+config = configparser.ConfigParser()
+config.read('token.ini')
+TOKEN = config['VK_API']['access_token']
+
+class Candidate_Selection():
     
-    # def __init__(
-    #     self,
-    #     user_access_token,
-    #     user_id,
-    #     user_name,
-    #     user_surname,
-    #     user_link,
-    #     user_age,
-    #     user_sex,
-    #     user_city,
-    #     version = '5.131',
-    #     album = 'profile',
-    #     extendet = False,
-    #     rev = True,
-    #     count = 3
-    #     ):
-    #     self.UAT = user_access_token
-    #     self.UID = user_id
-    #     self.UN = user_name
-    #     self.USN = user_surname
-    #     self.UL = user_link
-    #     self.UA = user_age
-    #     self.US = user_sex
-    #     self.UC = user_city
-    #     self.V = version
-    #     self.A = album
-    #     self.EX = extendet
-    #     self.R = rev
-    #     self.CNT = count
+    def __init__(
+        self,
+        user_age,
+        user_sex,
+        user_city,
+        user_access_token = TOKEN,
+        version = '5.131',
+        album = 'profile',
+        extended = int(True),
+        rev = int(False)
+        ):
+        self.UA = user_age
+        self.US = user_sex
+        self.UC = user_city
+        self.UAT = user_access_token
+        self.V = version
+        self.A = album
+        self.EX = extended
+        self.R = rev
         
-    def get_params():
-    # функция, которая берёт json со страницы
+    def candidate_parametrs(self):
+        url = 'https://api.vk.com/method/users.search'
+        response = requests.get(
+            url,
+            params = {
+                'is_closed': False,
+                'can_access_closed': False,
+                'access_token': self.UAT,
+                'fields': 'city, bdate',
+                'age_from': self.UA - 3,
+                'age_to': self.UA + 3,
+                'hometown': self.UC,
+                'sex': int(not(self.US)),
+                'v': '5.131',
+                'count': 1
+            }
+        )
+        return response.json()
+
+        
+    def get_parametrs_to_search(self):
+        for own_id in self.candidate_parametrs().values():
+            candidat_id = own_id['items'][0]['id']
         url = 'https://api.vk.com/method/photos.get'
         # вызываем и вводим параметры вызова
         response = requests.get(
             url,
             params = {
-            'access_token': 'vk1.a.xV9SUyov3GT2KqLJny5H1LEaJ1fkmWY5w29S1sfEzXsJ8wbjy1rUX4IPBgsj_QGfJ24tu-F62FB-qDpzm8kGNRcd2YRaX5S5n2I7zUW0B6PljetGcH7hTnCl14aLd8YWCR4-KUlIuBdiMePiWWtKD_xsS88Xm1ZBvPveM-uFuisOJksyZq1Ra6bQdc1GMrlr8-_aQ0zcjSLv44x1AdRLeg',#self.UAT,
-            'owner_id': '31539255',
-            'v': '5.131',#self.V,
-            'album_id': 'profile',#self.A,
-            'extended': True,#self.EX,
-            'rev': False,#self.R,
-            # 'count': 77#self.CNT
-            })
+            'access_token': self.UAT,
+            'owner_id': candidat_id,
+            'v': self.V,
+            'album_id': self.A,
+            'extended': self.EX,
+            'rev': self.R,
+            }
+        )
         return response.json()
 
-    def candidate_photo():
+    def candidate_photo(self):
         # метод выборки фотографий с наибольшей популярностью
         list_likes = []
         list_info = []
         url_list = []
         best_photo = []
-        for item in Interaction.get_params().values():
+        for item in self.get_parametrs_to_search().values():
             for inside_params in item['items']:
                 list_likes.append(inside_params['likes'].get('count'))
                 list_info.append(inside_params['sizes'])
                 # вот на этом моменте выделяется фотография со всеми размерами и ссылками по наибольшему количеству лайков
-                # в дальнейшем нет нужны фильтровать размеры фотографий, так как через return возвращаем автоматически
+                # в дальнейшем нет нужды фильтровать размеры фотографий, так как через return возвращаем автоматически
                 # наибольшее значение (главное расположить строку "url_list.append(url)" в нужном столбце)
                 info_photo_likes = sorted(dict(zip(list_likes, list_info)).items(), reverse=True)[:3]
             for items in info_photo_likes:
@@ -69,22 +85,10 @@ class Interaction():
                     url = i['url']
                 url_list.append(url)
         return {'photo': url_list}
-    
-    def candidat_info():
-        url = 'https://api.vk.com/method/users.get'
-        response = requests.get(
-            url,
-            params = {
-                'access_token': 'vk1.a.xV9SUyov3GT2KqLJny5H1LEaJ1fkmWY5w29S1sfEzXsJ8wbjy1rUX4IPBgsj_QGfJ24tu-F62FB-qDpzm8kGNRcd2YRaX5S5n2I7zUW0B6PljetGcH7hTnCl14aLd8YWCR4-KUlIuBdiMePiWWtKD_xsS88Xm1ZBvPveM-uFuisOJksyZq1Ra6bQdc1GMrlr8-_aQ0zcjSLv44x1AdRLeg',
-                'user_ids': '31539255',
-                'fields': ['bdate', 'sex', 'city'],
-                'v': '5.131'
-            })
-        return response.json().get('response')
 
-def func():
-    for i in Interaction.candidat_info():
-        i.update(Interaction.candidate_photo())
-        return i
-    
-print(func())    
+    def unification_info(self):
+        get_params_info = self.candidate_parametrs().get('response')['items'][0]
+        get_params_info.update(self.candidate_photo())
+        return get_params_info
+
+print(Candidate_Selection(18, bool(0), 'Севастополь').unification_info())
